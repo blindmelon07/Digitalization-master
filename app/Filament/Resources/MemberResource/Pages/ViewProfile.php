d6fd399921dfb7919b53fd0c74178c3d703c9dd2
bryan <bryandetera07@gmail.com>
2023-09-19 08:45:54 +0800

    Feat:Merge 1 and 2 branch

---
+++ app/Filament/Resources/MemberResource/Pages/ViewProfile.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\MemberResource\Pages;
+
+use App\Filament\Resources\MemberResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewProfile extends ViewRecord
+{
+    protected static string $resource = MemberResource::class;
+
+
+
+
+
+
+}
